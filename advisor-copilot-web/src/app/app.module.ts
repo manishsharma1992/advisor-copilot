c459c6356515f94af7d4e662bf0bc8ce14a46884
Ganesh <rganesh.info@gmail.com>
2023-10-13 16:25:39 +0530

    UI Development for Advisor- co pilot

--- advisor-copilot-web/src/app/app.module.ts
+++ advisor-copilot-web/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SpeechToTextComponent } from './speech-to-text/speech-to-text.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SpeechToTextComponent
+    SpeechToTextComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- advisor-copilot-web/src/app/speech-to-text/speech-to-text.component.ts
+++ advisor-copilot-web/src/app/speech-to-text/speech-to-text.component.ts
@@ -9,21 +9,34 @@ import { VoiceRecognitionService } from '../_services/voice-recognition.service'
 })
 export class SpeechToTextComponent {
   text: string = '';
+  showHeart = false;
+  reverseIcon: boolean = true;
+  currentTimestamp!: number;
+  currentTime!: string;
+  isClicked: boolean = true;
+
 
   constructor(
     public service : VoiceRecognitionService
-  ) { 
+  ) {
     this.service.init()
    }
+   toggleIcons() {
+    this.showHeart = !this.showHeart;
+  }
 
   ngOnInit(): void {
   }
 
   startService(){
     this.service.start()
+    const now = new Date();
+    this.currentTime = now.toLocaleTimeString();
+    this.isClicked = !this.isClicked;
   }
 
   stopService(){
     this.service.stop()
+      this.isClicked = !this.isClicked;
   }
 }
