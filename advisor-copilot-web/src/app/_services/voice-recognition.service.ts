bfb93b35f9065d17c3c8129c27759e24d4945d9b
Manish Sharma <mssharmam51@gmail.com>
2023-10-13 07:36:02 +0530

    added logic for speech recognition

---
+++ advisor-copilot-web/src/app/_services/voice-recognition.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+
+declare var webkitSpeechRecognition: any;
+@Injectable({
+  providedIn: 'root'
+})
+export class VoiceRecognitionService {
+
+  recognition =  new webkitSpeechRecognition();
+  isStoppedSpeechRecognition = false;
+  public text = '';
+  tempWords: any;
+
+  constructor() { }
+
+  init() {
+
+    this.recognition.interimResults = true;
+    this.recognition.lang = 'en-US';
+
+    this.recognition.addEventListener('result', (e: any) => {
+      const transcript = Array.from(e.results)
+        .map((result: any) => result[0])
+        .map((result: any) => result.transcript)
+        .join('');
+      this.tempWords = transcript;
+      console.log(transcript);
+    });
+  }
+
+  start() {
+    this.isStoppedSpeechRecognition = false;
+    this.recognition.start();
+    console.log("Speech recognition started")
+    this.recognition.addEventListener('end', (condition: any) => {
+      if (this.isStoppedSpeechRecognition) {
+        this.recognition.stop();
+        console.log("End speech recognition")
+      } else {
+        this.wordConcat()
+        this.recognition.start();
+      }
+    });
+  }
+  stop() {
+    this.isStoppedSpeechRecognition = true;
+    this.wordConcat()
+    this.recognition.stop();
+    console.log("End speech recognition")
+  }
+
+  wordConcat() {
+    this.text = this.text + ' ' + this.tempWords + '.';
+    this.tempWords = '';
+  }
+}
---
+++ advisor-copilot-web/src/app/speech-to-text/speech-to-text.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { VoiceRecognitionService } from '../_services/voice-recognition.service';
+
+@Component({
+  selector: 'app-speech-to-text',
+  templateUrl: './speech-to-text.component.html',
+  styleUrls: ['./speech-to-text.component.scss'],
+  providers: [VoiceRecognitionService]
+})
+export class SpeechToTextComponent {
+  text: string = '';
+
+  constructor(
+    public service : VoiceRecognitionService
+  ) { 
+    this.service.init()
+   }
+
+  ngOnInit(): void {
+  }
+
+  startService(){
+    this.service.start()
+  }
+
+  stopService(){
+    this.service.stop()
+  }
+}
